0fe280efeba1a6ead9599dede52c042addf0a409
julianParrax <julianfranco.parra@gmail.com>
2023-03-29 19:31:49 -0300

    ConexionesDB: Added: 1. configuracion inicial del proyecto

---
+++ dominio/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    //en esta parte del proyecto se reutilizan proyectos de dominio y negocio de un proyecto anterior
+    //para reutilizar la misma logica y adaptarla  progresivamente al ambito web
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        [DisplayName("Número")]
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+        public Elemento Tipo { get; set; }
+        public Elemento Debilidad { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
