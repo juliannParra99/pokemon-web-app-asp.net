0c90155e026eafc246bdad820668d2ca716fa782
julianParrax <julianfranco.parra@gmail.com>
2023-06-24 18:04:20 -0300

    Added: 5. seteos de grilla de pokemons, paginado y form de alta de pokemon

--- pokemon-web-app/pokemon-web/default.aspx
+++ pokemon-web-app/pokemon-web/default.aspx
@@ -8,33 +8,10 @@
     
     <div class="row row-cols-1 row-cols-md-3 g-4">
 
-    <%--<%foreach ( dominio.Pokemon poke in ListaPokemon)
-        {%>
 
-        <div class="col">
-            <div class="card h-100">
-                <img src="<%:poke.UrlImagen %>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><%:poke.Nombre %></h5>
-                    <p class="card-text"><%:poke.Descripcion %></p>
-                    <a href="pokemonDetalles.aspx?id=<%:poke.Id %> " class="btn btn-primary">Ver Detalle</a>
-                </div>
-            </div>
-        </div>
-    <%
-        } %>--%>
-
-        <%--ejemplo utulizando repeater: comento el foreach--%>
+        <%--ejemplo utulizando repeater: --%>
         <asp:Repeater ID="repRepetidor" runat="server">
-            <%-- itemTemplate: la parte del codigo que se va a repetir(equivalente a lo que esta dentro del foreach)
-                
-                Aa diferencia del forEach donde podemos trabajar con el objeto como tal. en el repeater
-                tenemos que llamar a las propiedades de ese objeto, despues de asignarlo como fuente
-                del repeater en el code behind: para usar esas propiedades tenemos que
-                colocarlas dentro de un <%#Eval("propiedad")%> ver el #, es importante.--%>
-
-            <%--con el repeater puedo conseguir una variable de esta pantalla y traerlo en esta pantalla de nuevo y hacer algo--%>
-            <%-- ademas podemos llamar al evento de un boton para capaturar el valor por ejemplo --%>
+            
             <ItemTemplate>
                 <div class="col">
             <div class="card h-100">
@@ -43,14 +20,7 @@
                     <h5 class="card-title"><%#Eval("Nombre") %></h5>
                     <p class="card-text"><%#Eval("Descripcion ")%></p>
                     <a href="pokemonDetalles.aspx?id=<%#Eval("Id") %> " class="btn btn-primary">Ver Detalle</a>
-                    <%--un bton para capturar un valor y reutilizarlo en la misma pantalla: el boton va a ir al evento
-                        click del boton en esta pantalla
-                        
-                        Para capturar el valor: utilizo el commandArgument que va a capturar el valor, y es el nombre de la 
-                        propiedad del objeto que va a capturar, en este caso el Id.Al argumento lo voy a guardar en una variable
-                         al momento en que llegue al code behind mediante el commandName, para manipularo en el back
-                        Capturar el valor ocurre mediante el evento onClick--%>
-                    <%-- el COMMANDArgument se pasa entre comillas simples --%>
+                    
                     <asp:Button Text="Ejemplo" ID="btnEjemplo" CssClass="btn btn-danger" CommandArgument= '<%#Eval("Id") %>' CommandName="PokemonId" OnClick="btnEjemplo_Click" runat="server" />
                 </div>
             </div>
--- pokemon-web-app/pokemon-web/default.aspx.cs
+++ pokemon-web-app/pokemon-web/default.aspx.cs
@@ -9,20 +9,7 @@ using dominio;
 
 namespace pokemon_web
 {
-    //En este ejemplo se va ejjemplificar la diferencia entre listar con repeater  y utilizar un foreach
-    //para poder repetir codigo.
-
-    //cual usemos dependera de lo qu queramos construir: por ejemplo, no siempre vamos a querer usar un repetear
-    //por que no siempre vmos a querer  almaacenar un valor de un objeto y ejecutar el el evento load
-    //para manipularlo en la misma pestaña. El for each no permite esto, pero al manjerar directamente el objeto
-    //podemos trabajar con distintas propiedades al mismo tiempo
-
-    //el repeater permite repetir codigo, pero ademas tiene otras funcionalidades, que permite capturar
-    //los valores de un objeto y guardarlo en una variable y utilizarlo en la misma pagina para crear
-    //una nueva funcionalidad dentro de la misma pagina. Como cuando mandamos una variable por url para
-    //relizar una funcionalidad. Pero con el repeater lo que pasa es que cuando se ejecuta el evento y se 
-    //guarda la variable se recarga el evento load, pero tenemos que validarlo para que guarde el valor
-    //utilizando el !IsPostBack, por que se recaga la pagina pero con el valor que se guardo
+    
     public partial class _default : System.Web.UI.Page
     {
         public List<Pokemon> ListaPokemon { get; set; }
@@ -33,8 +20,7 @@ namespace pokemon_web
             PokemonNegocio negocio = new PokemonNegocio();
 
             ListaPokemon = negocio.listaConSP();
-            //con esto le asigno la lista al repetidor para despues acceder a sus atributos y metodos.
-            //Similar a como es en el DGV
+            
 
             //queremos que se cargue el repetidor si no es posback, osea solamente la primera vez que se ejecuta. 
             if (!IsPostBack)
@@ -48,17 +34,9 @@ namespace pokemon_web
         //que va en el parametro.El sender es el elemento que envia el evento, en este caso un button
         protected void btnEjemplo_Click(object sender, EventArgs e)
         {
-            //yo se que el sender es un button asique le hago casteo explicito, por que es un object. Ademas, lo envuelvo entre otros parentesis para 
-            //indicar que es un boton, asique accedo a la propiedad de ese button.Con la propiedad CommandArgument guardamos
-            //el valor que conseguimos como argumento, lo guarda ahi.
-
-            //importante:cuando se ejecute el evento, se va a traer el dato y se va a generar un postaback, por lo que 
-            //se va  avolver a recargaar todo y  el repetidor a volver a recargarse en postback se v a arompar, por lo
-            //que solo cargamos los valores del repetidos si no es postback,  por que con que lo hagan la primera vez ya esta
-            //sino se duplican los valores: esa validacion se hace en el load
+            
             string valor = ((Button)sender).CommandArgument;
-            //valor capturaa el valor del id del pokemon, y lo podriamos usar para manejar esa variable en la misma pestaña
-            //y manejar logica como hacer un filtro. pero en este caso no lo usamos
+            
         }
     }
 }
