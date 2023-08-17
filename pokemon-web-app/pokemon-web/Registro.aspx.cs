d73b5edd20ce1480c760f952e4f150316af1ea62
julianParrax <julianfranco.parra@gmail.com>
2023-08-17 19:12:26 -0300

    Added: 15. Autologin en el registro +  configuracion de acceso a pantallas + boton de salir si estas logueado

--- pokemon-web-app/pokemon-web/Registro.aspx.cs
+++ pokemon-web-app/pokemon-web/Registro.aspx.cs
@@ -7,7 +7,14 @@ using System.Web.UI.WebControls;
 using dominio;
 using negocio;
 
+//En este ejemplo se va crear la funcionalidad de hacer que cuando un usuario se registrar quede la session guardada directamente y lo
+//redirija al home directamente .
 
+//Ademas, en esta seccion se terminan ciertas funcionalidades que quedaron pendientes de la seccion anterior, la 
+//tarea era:  no se debe de restringir la vista del home y la pantalla de registrarse aunque no este logead. Si hay una
+//session activa o no habilitar o no los botones de login y registrarse dependiendo de si estoy logueeado o no, osea,
+//y si hay una seccion activa no voy a tener disponible el boton registrarse y loguearse, y que se muestre un boton salir,
+//que permitiria desloguearse.
 
 namespace pokemon_web
 {
@@ -32,12 +39,20 @@ namespace pokemon_web
                 user.Pass = txtPassword.Text;
 
 
-                //Obtiene el id de usuario que se acaba de generar para disponer de sus datos
-                int id = traineeNegocio.insertarNuevo(user);
+                //Obtiene el id de usuario que se acaba de generar para disponer de sus datos, y lo asigna al usuario que va a estar vivio
+                //en la sesion despues. si el usuario tuviera mas datos tambien habria que asignarlos, pero con esto estamos bien por ahora
+                //por que solo usamos email y password. Lo que pdoria ahcer tambiene s llamar al metodo login y traer los datos de db para 
+                //iniciar session, pero seria unn poco redundante
+                user.Id = traineeNegocio.insertarNuevo(user);
+                //agrega el usuario que se acaba de generar a la session para que interactue con la aplicacion. 
+                Session.Add("trainee", user);
 
-                emailService.armarCorreo(user.Email, "Bienvenida Trainee", "Hola te damos la bienvenida a la aplicación...");
 
-                emailService.enviarEmail();
+                //IMP: TENGO QUE CONFIGURAR CORRECTAMENTE EL ENVIO DE MAILS, SINO DA UNA EXCEPCION Y NO SE REDIRIJE A LA PAGINA DEFAULT
+                //UNA VEZ EL USUARIO SE REGISTRA
+                //emailService.armarCorreo(user.Email, "Bienvenida Trainee", "Hola te damos la bienvenida a la aplicación...");
+
+                //emailService.enviarEmail();
 
                 // Se redirige al usuario a la página "Default.aspx" y se establece el parámetro 'false' para que no se realice un envío inmediato de la respuesta al cliente.
                 Response.Redirect("default.aspx", false);
--- pokemon-web-app/pokemon-web/masterPage.Master.cs
+++ pokemon-web-app/pokemon-web/masterPage.Master.cs
@@ -12,9 +12,7 @@ namespace pokemon_web
     public partial class masterPage : System.Web.UI.MasterPage
     {
 
-        //utilizamos la pagina maestra que es heradada a las otras para que, cuando se carge una pagina, se verifique si el usuario tiene
-        //Permitido por la, seguridad de la aplicacion, acceso a las pestañas.
-
+        //utilizamos la pagina maestra que es heradada a las otras 
         // Evento Page_Load que se ejecuta cuando la página se carga.
 
         protected void Page_Load(object sender, EventArgs e)
@@ -25,9 +23,9 @@ namespace pokemon_web
             //seria algo como :  si la pagina que va a contener el masterPage es la page Login, entonces...
             //Si la pagina que estoy por cargar no es login, entonces que chequee la seguridad
 
-            //tarea: con esto todas las paginas que no sean login no las voy a poder ver y me va a redirigir a login. Por lo que en esta condicion
-            //tengo que  colocar para que se puedan ver El home, el registrarse y el login
-            if (!(Page is Login))
+             //para que se puedan ver El home, el registrarse y el login
+             //si no es ninguna de esas paginas va a redirigir al login para iniciar sesion
+            if (!(Page is Login || Page is Registro || Page is _default))
             {
                 // Verificamos si no hay una sesión activa para el usuario en la variable de sesión "trainee".
                 if (!Seguridad.sesionActiva(Session["trainee"]))
@@ -39,22 +37,18 @@ namespace pokemon_web
 
 
         }
+
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
+            //con esto borro todos los datos del usuario de la session.. Podria solo buscar los datos del objeto trainee
+            //pero como no quiero guardar nada del usuario borro todo
+            //Session.Clear() borra todos los datos almacenados en la sesión actual.La "sesión" es una forma de mantener datos
+            //entre diferentes solicitudes de un mismo usuario en una aplicación web.Borrar la sesión implica que se eliminarán
+            //todos los datos almacenados en ella, como información de inicio de sesión, preferencias del usuario, etc.
+            Session.Clear();
+            Response.Redirect("Login.aspx");
+        }
     }
 }
 
 
-//En este código, se define una clase llamada masterPage que hereda de System.Web.UI.MasterPage. Esta clase está relacionada con la lógica
-//    de una página maestra en ASP.NET. En el evento Page_Load, se ejecuta cuando la página se carga.
-
-//if (!(Page is Login)): Esta línea verifica si la página actual no es la página de inicio de sesión (Login).
-//    El operador !(Page is Login) verifica si la página no es una instancia de la clase Login.
-
-//if (!Seguridad.sesionActiva(Session["trainee"])): Si la página no es la página de inicio de sesión y no
-//    hay una sesión activa para el usuario (verificado utilizando el método sesionActiva de la clase Seguridad y la variable de sesión "trainee"),
-//    se ejecuta el bloque de código dentro de este if.
-
-//Response.Redirect("Login.aspx", false);: Si no hay una sesión activa, se redirige al usuario a la página de inicio de sesión (Login.aspx).
-//    El segundo parámetro false indica que no se debe detener el flujo de ejecución después de la redirección, lo que permite que el código continúe ejecutándose después de la redirección.
-
-//En resumen, este código se utiliza en una página maestra para verificar si el usuario tiene una sesión activa. Si el usuario no tiene una 
-//sesión activa y la página actual no es la página de inicio de sesión, se redirige al usuario a la página de inicio de sesión.
